e2b35ea15250fa41740d2aa33d0ac370db1831d6
chetan <chetan.shinde@conditation.com>
2019-04-15 11:38:11 +0530

    make some changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+
 class App extends Component {
+  
+ state={
+   Person:[
+   {name:"chetan", age:20},
+   {name:"harshal", age:22},
+   {name:"rakesh", age:25},
+   {name: "rahul", age:26},
+   {name: "rahul", age:26},
+   {name: "rahul", age:26}
+    
+  
+  ]
+ }
+
+ eventHandler(){
+
+console.log("clicked");
+
+//  alert("click by user");
+
+ }
+
   render() {
     return (
       <div className="App">
         <h1>Hello welcome to react js tutorial</h1>
     <p> this is really working!!!!!</p>
-     <Person/>
+   
+
+  <button onClick={this.eventHandler}> Switch Button </button>
+  <Person name={this.state.Person[0].name} age={this.state.Person[0].age}> </Person>
+  <Person name={ this.state.Person[1].name} age={this.state.Person[1].age}></Person>
+  <Person name={this.state.Person[2].name} age={ this.state.Person[2].age}></Person>
+<Person name={this.state.Person[3].name } age={this.state.Person[3].age}></Person>
+ 
+    {/* <Person name="chetan" age="20"/> 
+    <Person name="rahul" age="22"/>
+    <Person name="harshal" age="24">my Hobbies: racing </Person>
+    <Person name="rakesh" age="26"/> */}
+    
        </div>
       
     );
@@ -18,4 +53,3 @@ class App extends Component {
 }
 
 export default App;
-
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,16 @@
 import React from 'react';
+const Person = (props) => {
 
-const Person = () => {
+//    return <p> Hello my name chetan and i am {Math.floor(Math.random() * 30 ) } year old </p>
 
-    return <p> my name is chetan</p>
+
+return(
+<div>
+     <p> Hello my name is {props.name} and i am {props.age}  year old </p>
+     <p> {props.children}</p>
+ 
+ </div>
+);
 }
 
 export default Person;
