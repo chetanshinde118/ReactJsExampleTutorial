67353cf8eb5a9801120f080c3b0114d979318b9e
chetanshinde118 <chetan.shinde@coditation.com>
2019-04-25 16:14:44 +0530

    to diplay the data using map (outputting lists)

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import './Person.css';
 const Person = ( props ) => {
 
     return(
-  <div className = " Person ">
-      <p>i am {props.name} and i am {props.age} year old</p>
-   <p> {props.children}</p>
-  
+  <div className = "persons">
+      <p onClick= {props.click}> My name is {props.name} and i am {props.age} year old</p>
+      <p> {props.children}</p>
+      <input type= "text"onChange= {props.changed} value={props.value}/>  
   </div>
          
     )
